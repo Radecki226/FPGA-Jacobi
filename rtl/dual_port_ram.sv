5fc590ae6b730fb9af824de9a191965d2a12dffe
radecki226 <radecki226@gmail.com>
2022-05-25 12:02:41 +0200

    cleaning

--- rtl/dual_port_ram.sv
+++ rtl/dual_port_ram.sv
@@ -16,7 +16,7 @@ module dual_port_ram
   input                       we_a,
   input      [ADDR_WIDTH-1:0] addr_a,
   input      [DATA_WIDTH-1:0] din_a,
-  output     [DATA_WIDTH-1:0] dout_a,
+  output reg [DATA_WIDTH-1:0] dout_a,
 
   //port B
   input                       clk_b,
@@ -24,48 +24,32 @@ module dual_port_ram
   input                       we_b,
   input      [ADDR_WIDTH-1:0] addr_b,
   input      [DATA_WIDTH-1:0] din_b,
-  output     [DATA_WIDTH-1:0] dout_b  
+  output reg [DATA_WIDTH-1:0] dout_b  
    
 );
   
-  reg [DATA_WIDTH-1:0] ram [MEM_SIZE-1:0]; 
-  reg [ADDR_WIDTH-1:0] addr_a_r;
-  reg [ADDR_WIDTH-1:0] addr_b_r;
-  reg [DATA_WIDTH-1:0] dat_a_r;
-  reg [DATA_WIDTH-1:0] dat_b_r;
+  reg  [DATA_WIDTH-1:0]  ram [MEM_SIZE-1:0]; 
+
   
   // port A logic
-  always_ff @(posedge clk_a) begin
+  always @(posedge clk_a) begin
     if (en_a) begin
       if (we_a) begin
         ram[addr_a] <= din_a;
       end
-      addr_a_r <= addr_a;
+      dout_a <= ram[addr_a];
     end
   end
-
-  always_ff @(posedge clk_a) begin
-    if (en_a) begin
-      dat_a_r <= ram[addr_a_r];
-    end
-  end
-  assign dout_a = dat_a_r;
 
   // port B logic
-  always_ff @(posedge clk_b) begin
+  always @(posedge clk_b) begin
     if (en_b) begin
       if (we_b) begin
         ram[addr_b] <= din_b;
       end
-      addr_b_r <= addr_b;
+      dout_b <= ram[addr_b];
     end 
   end
-  
-  always_ff @(posedge clk_b) begin
-    if (en_b) begin
-      dat_b_r <= ram[addr_b_r];
-    end
-  end
-  assign dout_b = dat_b_r;
- endmodule
 
+
+ endmodule
